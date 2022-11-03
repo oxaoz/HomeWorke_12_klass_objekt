857d8e69f638110608bea4383e5e10087cac739b
oxaoz <ogozer@ya.ru>
2022-11-04 03:02:10 +0700

    Проба пера

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String bookM = "MUMU";
+        String writerNameM = "Ivan Turgenev";
+        int publisherYerM = 2002;
+
+        String bookK = "Kolobok";
+        String writerNameK = "Russian People";
+        int publisherYearK = 1993;
+
+
+        String [] names = {"MUMU", "Kolobok"};
+        String[] writerNames = {"Ivan Turgenev", "Russian People"};
+        int[] publisherYears = {2002, 1993};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Книга - " + names[i] + ", автора " + writerNames[i] + ", издана в " + publisherYears[i] + " году.");
+        }
+        }
+
     }
-}
